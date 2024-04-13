85340b99034364019887b8630df1c1d8dad303ec
Anika Mathur <111272336+mathuranika@users.noreply.github.com>
2024-04-13 16:50:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import './App.css';
 import Home from './Home';  
 import Header from './Header';
 import Footer from './Footer';
-import SearchPage from './SearchPage'
-import { Route, Switch } from 'wouter';
+
 
 function App() {
   return (
     <div className="app">
       <Header/>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/search" component={SearchPage} />
-      </Switch>
+      <Home/>
       <Footer/>
     </div>
   );
